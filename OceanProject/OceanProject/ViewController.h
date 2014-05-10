a0195b25d2b3eb1d0313f19e34bc11e1b2461b1c
Louise Whitney <louisewhitney@fullsail.edu>
2014-05-10 19:12:31 -0400

    Initial Commit

---
+++ OceanProject/OceanProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OceanProject
+//
+//  Created by Louise Whitney on 5/10/14.
+//  Copyright (c) 2014 Louise Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OceanProject/OceanProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OceanProject
+//
+//  Created by Louise Whitney on 5/10/14.
+//  Copyright (c) 2014 Louise Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
